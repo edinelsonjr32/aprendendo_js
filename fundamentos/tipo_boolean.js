b417869095171b0c25b39d910ec7f215b0fb1b7f
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-23 14:51:06 -0300

    Boolean

---
+++ fundamentos/tipo_boolean.js
@@ -0,0 +1,19 @@
+const estudante = 'Luis'
+
+const estaAprovado = false
+
+if (estaAprovado){
+    console.log(`Parabéns ${estudante}, você está aprovado`);
+    
+}else{
+    console.log(`Lamentamos ${estudante}, você está reprovado`);
+
+}
+
+if (estudante === 'Luis'){
+    console.log(`Olá, ${estudante}`);
+    
+}else{
+    console.log('Quem é você?');
+    
+}
