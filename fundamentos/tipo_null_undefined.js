ab948fca25b3c47b9806d6db40f4683d635f8f8b
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-23 14:56:51 -0300

    null e undefined

---
+++ fundamentos/tipo_null_undefined.js
@@ -0,0 +1,12 @@
+let nomeEstudante;
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+
+let telefoneEstudante = null
+
+console.log(telefoneEstudante);
+console.log(typeof telefoneEstudante);
+
+console.log(nomeEstudante + 3);
+console.log(telefoneEstudante + 3);
+
