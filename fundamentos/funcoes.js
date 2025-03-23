3e202528bda620d9f7906b4d7cd100f53958e92f
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-23 16:38:39 -0300

    Relembrando funções

---
+++ fundamentos/funcoes.js
@@ -0,0 +1,13 @@
+//parâmetros/argumentos
+//retorno
+
+function exibeNomeEstudante(nome, nota){
+    console.log(`Olá ${nome} e a nota ${nota}`)
+}
+function retornaNomeEstudante(nome, nota){
+    return `Olá ${nome} e a nota ${nota}`
+}
+
+exibeNomeEstudante('Edinelson', 9.5)
+console.log(retornaNomeEstudante('João', 20));
+
