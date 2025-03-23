f6a015c6327933c686c86301a18fae447978281e
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-23 15:16:28 -0300

    Truthy e Falsy

--- fundamentos/operadores_logicos.js
+++ fundamentos/operadores_logicos.js
@@ -1,9 +1,20 @@
 const notaFinal = 6.99
 const faltas = 5
 
+const advertencia = 0
+
 if (notaFinal < 7 && faltas > 4){
     console.log('Reprovado');
     
 }else{
     console.log('Não foi reprovado');
+}
+
+
+if (faltas >= 2 && !advertencia){
+    console.log('Recebeu bonus');
+    
+}else{
+    console.log('Não Recebeu bonus');
+
 }
