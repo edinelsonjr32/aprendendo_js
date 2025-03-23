4cec7b492a3d720630af79957db06550cc080a14
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-23 16:44:24 -0300

    Expressão de função

---
+++ fundamentos/expressao_funcao.js
@@ -0,0 +1,14 @@
+
+
+
+const estudanteReprovou = function (notaFinal, faltas){
+    if(notaFinal < 7 && faltas > 4){
+        return true
+    }else{
+        return false
+    }
+}
+//hoisting
+
+console.log(estudanteReprovou(4, 5));
+console.log(estudanteReprovou(8, 4));
