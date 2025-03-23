9a7ac5641d8277afe76c9f423ea0e902310c53ab
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-23 14:21:01 -0300

    Escopo em Variaveis

---
+++ fundamentos/escopo.js
@@ -0,0 +1,15 @@
+/**
+ * Escopo em JS
+ * 
+ * Bloco: Preso em um bloco
+ * Global: elas podem sempre ser acessadas ou modificadas dentro dos blocos.
+ * Função: 
+ */
+let estudante;
+
+if (1 > 0){
+    estudante = 'Edi'
+    console.log(estudante);
+}
+
+console.log(estudante);
