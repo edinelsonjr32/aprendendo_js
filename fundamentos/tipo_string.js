e7cc375ba5b719817373de7255a3159d94c13b39
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-23 14:44:58 -0300

    String

---
+++ fundamentos/tipo_string.js
@@ -0,0 +1,18 @@
+const estudante = 'Edinelson'
+const docente = 'Junior'
+
+const cumprimento = "Nosso lema é: 'Estudar Bastante!'"
+
+
+const citacao = `Nosso lema é: 'Estudar Bastante!'`
+console.log(cumprimento);
+
+//template string
+console.log(`A estudante chama ${estudante}`);
+
+//operaçoes com string
+const senha = 'SenhaSegura123' + estudante.toUpperCase()
+console.log(senha)
+
+
+
