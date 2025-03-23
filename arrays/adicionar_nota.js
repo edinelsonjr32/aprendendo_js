ce024da1bac9936304300fe367e7beb4caa3052c
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-23 17:25:56 -0300

    Adicionando elementos

---
+++ arrays/adicionar_nota.js
@@ -0,0 +1,9 @@
+const notas = [10, 6, 8]
+//método para adicionar item no array = push(item), ele sempre adiciona no final do array
+notas.push(9)
+
+const media = (notas[0] +  notas[1] +  notas[2] +  notas[3]) / notas.length
+
+console.log(notas);
+console.log(media);
+
