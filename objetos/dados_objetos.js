806e818fe4973fb4c12aafe1d0920fadc624b889
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-24 10:07:54 -0300

    dados objetos

---
+++ objetos/dados_objetos.js
@@ -0,0 +1,14 @@
+const estudante = {
+    nome : 'Edinelson Junior',
+    idade: 30,
+    cpf: '01508999201',
+    turma: 'javascript',
+    bolsista: true,
+    telefone: [
+        '5593999989899',
+        '5593999989891',
+        '5593999989892',
+    ]
+}
+
+console.log(estudante.telefone[0]);
