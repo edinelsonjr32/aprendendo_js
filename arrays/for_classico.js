09b6b2a385227865633c0315e67f5e73ecfa7fcc
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-24 05:17:35 -0300

    for_classico

---
+++ arrays/for_classico.js
@@ -0,0 +1,9 @@
+const notas = [10, 8.5, 5, 6.5, 8, 7.5]
+
+//primeira expressão é executada apenas uma vez
+//segunda expressão é a expressão de condição até chegar no critério de parada
+//incremento até chegar no critério de parada
+for (let i=0; i < notas.length; i++){
+    //console.log(i, notas[i]);
+    console.log(`notas[${i}]: ${notas[i]}`);
+} 
