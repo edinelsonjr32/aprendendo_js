eaec2d1b4165e7d931c8220e30bdbcc2adf24854
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-24 06:01:20 -0300

    removendo duplicatas com Set

---
+++ arrays/remover_duplicatas.js
@@ -0,0 +1,10 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+//const nomesAtualizados = new Set(nomes);
+
+//transformar um conjunto em array
+
+const listaNomesAtualizados = [...new Set(nomes)]
+
+//console.log(nomesAtualizados);
+console.log(listaNomesAtualizados);
