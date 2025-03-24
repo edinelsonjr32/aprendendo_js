3e3192f477a86fcbf2de7e774fd91415ea2bbb14
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-24 10:11:56 -0300

    objetos alinhados

---
+++ objetos/objetos_alinhados.js
@@ -0,0 +1,23 @@
+const estudante = {
+    nome : 'Edinelson Junior',
+    idade: 30,
+    cpf: '01508999201',
+    turma: 'javascript',
+    bolsista: true,
+    telefone: [
+        '5593999989899',
+        '5593999989891',
+        '5593999989892',
+    ]
+}
+
+estudante.endereco = {
+    rua: 'Atalaia',
+    numero: '120',
+    complemento: 'apto 43',
+    cidade: 'Óbidos',
+    estado: 'Pará'
+}
+
+console.log(estudante);
+console.log(estudante.endereco.rua);
