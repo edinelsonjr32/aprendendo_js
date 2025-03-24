be0b3e8c0de990bcca44dd3e8d7f2223e0857f81
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-24 09:56:54 -0300

    Alerando objetos

---
+++ objetos/alterando_objetos.js
@@ -0,0 +1,21 @@
+const estudante = {
+    nome : 'Edinelson Junior',
+    idade: 30,
+    cpf: '01508999201',
+    turma: 'javascript'
+}
+estudante.telefone = "93-992076225"
+
+console.log(estudante);
+
+
+estudante.nome = "Edinelson Luis"
+console.log(estudante);
+
+//posso adicionar objeto vazio
+
+const estudante2 = {}
+
+estudante2.nome = 'Ana Maria'
+
+console.log(estudante2);
