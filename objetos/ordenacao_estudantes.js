67045febc808e9926c016cf73b2ca065791bd3e6
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-24 21:46:51 -0300

    Encontrando Objetos/Filtrando objetos

---
+++ objetos/ordenacao_estudantes.js
@@ -0,0 +1,18 @@
+const estudante = require('./estudantes.json')
+
+console.log(estudante);
+
+function ordena(lista, propriedade){
+    return lista.sort((a, b)=> {
+        if (a[propriedade] < b[propriedade]){
+            return -1
+        }
+        if (a[propriedade] > b[propriedade]){
+            return 1
+        }
+        return 0
+    })
+}
+const listaOrdenada = ordena(estudante, 'nome')
+
+console.log(listaOrdenada);
